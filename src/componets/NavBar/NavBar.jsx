29add370ceb503b0385fba7ad6858d2225395149
Jorge <116285852+JJtarazona@users.noreply.github.com>
2023-08-29 22:35:31 -0500

    Cambios de idioma - form en el modal2

--- src/componets/NavBar/NavBar.jsx
+++ src/componets/NavBar/NavBar.jsx
@@ -60,27 +60,28 @@ export default function NavBarC() {
           <DropdownTrigger>
             <Avatar
               isBordered
+              color="primary"
               as="button"
               className="transition-transform"
-              color="secondary"
               name="Jason Hughes"
               size="sm"
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              src="https://avatars.githubusercontent.com/u/116285852?v=4"
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="Profile Actions" variant="flat">
             <DropdownItem key="profile" className="h-14 gap-2">
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              <p className="font-semibold">Bienvenid@</p>
+              <p className="font-semibold">Jorge Tarazona</p>
             </DropdownItem>
-            <DropdownItem key="settings">My Settings</DropdownItem>
-            <DropdownItem key="team_settings">Team Settings</DropdownItem>
-            <DropdownItem key="analytics">Analytics</DropdownItem>
-            <DropdownItem key="system">System</DropdownItem>
-            <DropdownItem key="configurations">Configurations</DropdownItem>
-            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="settings">Configuración</DropdownItem>
+            <DropdownItem key="analytics">Pre-Agendamiento</DropdownItem>
+            <DropdownItem key="system">Historias e Informes</DropdownItem>
+            <DropdownItem key="configurations">Reportes Novedades</DropdownItem>
+            <DropdownItem key="help_and_feedback">
+              Tablerto informativo
+            </DropdownItem>
             <DropdownItem key="logout" color="danger">
-              Log Out
+              Cerrar Sesión
             </DropdownItem>
           </DropdownMenu>
         </Dropdown>
